56a3a9fdbc558eb0945fa8b40ebf9de37d3e94bc
gngrninja <thegngrninja@gmail.com>
2016-05-11 17:02:19 -0700

    Signed-off-by: gngrninja <thegngrninja@gmail.com>

--- ninjalogging.psm1
+++ ninjalogging.psm1
@@ -1,6 +1,5 @@
 Set-StrictMode -Version Latest
 
-
 $scriptPath = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
 $scriptName = (Get-ChildItem $MyInvocation.PSCommandPath | Select-Object -ExpandProperty BaseName)
 
@@ -11,6 +10,108 @@ if ($scriptName.count -gt 1) {
 }
 
 function New-LogFile {
+<#
+.SYNOPSIS
+   New-LogFile will create a log file.
+
+.DESCRIPTION
+   New-LogFile will create a log file. 
+
+   You can specify different paramaters to change the file's name, and where it is stored.
+   By default it will attempt to get the name of the calling function or script via $scriptName = (Get-ChildItem $MyInvocation.PSCommandPath | Select-Object -ExpandProperty BaseName).
+   It will also attempt to get the path via $scriptPath = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition.
+   You can also specify the path and name, as well as if you'd like to append the date in the following format: MM-dd-yy_HHmm.
+
+   Use the -Verbose parameter to display what is happening to the host.
+
+.PARAMETER logPath
+    Alias: Path
+    Type : String
+
+    Specify the path to the logfile
+
+.PARAMETER logName    
+    Alias: Name
+    Type : String
+
+    Specify the name of the log file. Be sure to include the extension if specifying the name.
+
+.PARAMETER scriptVersion
+    Type : Double
+
+    Specify the version of your script being run. If left blank, will default to 0.1
+
+.PARAMETER addDate
+    Type : Boolean
+
+    Specify if you'd like to add the date to the file name.  If you're specifying logName, you can use addDate to append the current date/time in the format: MM-dd-yy_HHmm.
+
+.NOTES
+    Name: New-LogFile
+    Version: 1.0
+    Author: Ginger Ninja (Mike Roberts)
+    DateCreated: 5/11/16
+    
+
+.LINK
+    http://www.gngrninja.com
+
+.EXAMPLE
+    $logFile = New-LogFile 
+    -----------------------------
+    
+    gngrNinja> $logFile
+    C:\PowerShell\logs\ScriptLog_05-11-16_1612.log
+
+.EXAMPLE
+    $logFile = New-LogFile -Verbose
+    -----------------------------
+    
+    VERBOSE: No path specified. Using: C:\PowerShell\logs
+    VERBOSE:
+    VERBOSE: No log name specified. Setting log name to: ScriptLog.log and adding date.
+    VERBOSE:
+    VERBOSE: Adding date to log file with an extension! New file name:
+    ScriptLog_05-11-16_1613.log
+    VERBOSE:
+    VERBOSE: Created C:\PowerShell\logs\ScriptLog_05-11-16_1613.log
+    VERBOSE:
+    VERBOSE: File C:\PowerShell\logs\ScriptLog_05-11-16_1613.log created and verified to
+    exist.
+    VERBOSE:
+    VERBOSE: Adding the following information to:
+    C:\PowerShell\logs\ScriptLog_05-11-16_1613.log
+    VERBOSE:
+    VERBOSE: -----------------------------------------------------------------
+    VERBOSE: Started logging at [05/11/2016 16:13:11]
+    VERBOSE: Script (Version 0.1) executed by: [thegn] on computer: [GINJA10]
+    VERBOSE: -----------------------------------------------------------------
+    VERBOSE:
+    gngrNinja>
+
+.EXAMPLE
+    $logfile = New-LogFile -Name 'testName.log' -path  'c:\temp' -addDate $true -Verbose
+    -----------------------------
+
+    VERBOSE: Adding date to log file with an extension! New file name:
+    testName_05-11-16_1615.log
+    VERBOSE:
+    VERBOSE: Created c:\temp\testName_05-11-16_1615.log
+    VERBOSE:
+    VERBOSE: File C:\temp\testName_05-11-16_1615.log created and verified to exist.
+    VERBOSE:
+    VERBOSE: Adding the following information to: C:\temp\testName_05-11-16_1615.log
+    VERBOSE:
+    VERBOSE: -----------------------------------------------------------------
+    VERBOSE: Started logging at [05/11/2016 16:15:13]
+    VERBOSE: Script (Version 0.1) executed by: [thegn] on computer: [GINJA10]
+    VERBOSE: -----------------------------------------------------------------
+    VERBOSE:
+
+.OUTPUTS
+    Full path to the log file created.
+#>
+[cmdletbinding()]
     [cmdletbinding()]
     param(
         [Parameter(Mandatory = $false,
@@ -166,13 +267,13 @@ function New-LogFile {
         Write-Verbose "Adding the following information to: $fullPath"
         Write-Verbose ""
         Write-Verbose ('-'*$flairLength)
-        Write-Verbose "Started processing at [$time]"
+        Write-Verbose "Started logging at [$time]"
         Write-Verbose "Script (Version $scriptVersion) executed by: [$curUser] on computer: [$curComp]"
         Write-Verbose ('-'*$flairLength)
         Write-Verbose ""
         
         Add-Content -Path $fullPath -Value ('-'*$flairLength)
-        Add-Content -Path $fullPath -Value "Started processing at [$time]"
+        Add-Content -Path $fullPath -Value "Started logging at [$time]"
         Add-Content -Path $fullPath -Value "Script (Version $scriptVersion) executed by: [$curUser] on computer: [$curComp]"
         Add-Content -Path $fullPath -Value ('-'*$flairLength)
         Add-Content -Path $fullPath -Value ""
@@ -190,6 +291,105 @@ function New-LogFile {
 }
 
 function Write-LogFile {
+<#
+.SYNOPSIS
+   Write-LogFile will add information to a log file created with New-LogFile.
+
+.DESCRIPTION
+   Write-LogFile will add information to a log file created with New-LogFile.
+
+   By default additions to the log file will include a timestamp, unless you specify -addTimeStamp $false.
+   This function accepts values from the pipeline, as demonstrated in an example.
+
+   Use the -Verbose parameter to display what is being logged to the host.
+   
+.PARAMETER logPath
+    Alias: Path
+    Type : String
+
+    Specify the full path to the log file, including the name.
+
+.PARAMETER logValue
+    Alias: Value
+    Type : String
+
+    Specify the value(s) you'd like logged.
+
+.PARAMETER addTimeStamp
+    Type : Boolean
+
+    Defaults to true, set to false if you'd like to omit the timestamp.
+
+.NOTES
+    Name: Write-LogFile
+    Version: 1.0
+    Author: Ginger Ninja (Mike Roberts)
+    DateCreated: 5/11/16
+
+.LINK
+    http://www.gngrninja.com
+
+.EXAMPLE
+    For this example we'll assume you use:
+    $logFile = New-LogFile 
+
+    Write-LogFile -logPath $logFile -logValue 'test log value!'
+    -----------------------------
+
+    gngrNinja> more $logfile
+    -----------------------------------------------------------------
+    Started logging at [05/11/2016 16:19:37]
+    Script (Version 0.1) executed by: [thegn] on computer: [GINJA10]
+    -----------------------------------------------------------------
+
+    [05-11-16 16:23:24] test log value!
+
+.EXAMPLE
+    For this example we'll assume you use:
+    $logFile = New-LogFile 
+
+    Write-LogFile -logPath $logFile -logValue 'test log value!' -Verbose
+    -----------------------------
+    
+    VERBOSE: Adding [05-11-16 16:25:19] test log value! to
+    C:\PowerShell\logs\ScriptLog_05-11-16_1619.log
+    VERBOSE:
+
+.EXAMPLE
+    For this example we'll assume you use:
+    $logFile = New-LogFile 
+
+    Get-Process | Write-LogFile $logFile -Verbose
+    -----------------------------
+
+    ...
+    VERBOSE: Adding [05-11-16 16:26:51] System.Diagnostics.Process (wininit) to
+    C:\PowerShell\logs\ScriptLog_05-11-16_1619.log
+    VERBOSE:
+    VERBOSE: Adding [05-11-16 16:26:51] System.Diagnostics.Process (winlogon) to
+    C:\PowerShell\logs\ScriptLog_05-11-16_1619.log
+    VERBOSE:
+    VERBOSE: Adding [05-11-16 16:26:51] System.Diagnostics.Process (WmiPrvSE) to
+    C:\PowerShell\logs\ScriptLog_05-11-16_1619.log
+    VERBOSE:
+    VERBOSE: Adding [05-11-16 16:26:51] System.Diagnostics.Process (WmiPrvSE) to
+    C:\PowerShell\logs\ScriptLog_05-11-16_1619.log
+    VERBOSE:
+    VERBOSE: Adding [05-11-16 16:26:51] System.Diagnostics.Process (WUDFHost) to
+    C:\PowerShell\logs\ScriptLog_05-11-16_1619.log
+    VERBOSE:
+    ...
+
+.EXAMPLE
+    For this example we'll assume you use:
+    $logFile = New-LogFile 
+
+    Write-LogFile -logPath $logFile -logValue 'test without timestamp' -addTimeStamp $false -Verbose
+    -----------------------------
+
+    VERBOSE: Adding test without timestamp to C:\PowerShell\logs\ScriptLog_05-11-16_1631.log
+    VERBOSE:
+#>
     [cmdletbinding()]
     param(
         [Parameter(Mandatory = $true,
@@ -247,6 +447,66 @@ function Write-LogFile {
 }
 
 function Write-LogFileError {
+<#
+.SYNOPSIS
+   Write-LogFileError will add information to a log file created with New-LogFile. The information will be prepended with [ERROR].
+
+.DESCRIPTION
+   Write-LogFileError will add information to a log file created with New-LogFile. The information will be prepended with [ERROR].
+
+   By default additions to the log file will include a timestamp, unless you specify -addTimeStamp $false.
+   This function accepts values from the pipeline, as demonstrated in an example.
+
+   Use the -Verbose parameter to display what is being logged to the host.
+   
+.PARAMETER logPath
+    Alias: Path
+    Type : String
+
+    Specify the full path to the log file, including the name.
+
+.PARAMETER errorDesc
+    Alias: Value
+    Type : String
+
+    Specify the value(s) you'd like logged as errors.
+
+.PARAMETER addTimeStamp
+    Type : Boolean
+
+    Defaults to true, set to false if you'd like to omit the timestamp.
+
+.PARAMETER exitScript
+    Alias: Exit
+    Type : Boolean
+
+    This parameter let's you specify $true if you'd like to exit the script after the error is logged. 
+    It defaults to $false.
+    
+.NOTES
+    Name: Write-LogFileError
+    Version: 1.0
+    Author: Ginger Ninja (Mike Roberts)
+    DateCreated: 5/11/16
+
+.LINK
+    http://www.gngrninja.com
+
+.EXAMPLE
+    For this example we'll assume you use:
+    $logFile = New-LogFile 
+
+    Write-LogFileError -logPath $logFile -errorDesc 'test log value error!'
+    -----------------------------
+
+    gngrNinja> more $logFile
+    -----------------------------------------------------------------
+    Started logging at [05/11/2016 16:31:44]
+    Script (Version 0.1) executed by: [thegn] on computer: [GINJA10]
+    -----------------------------------------------------------------
+
+    [05-11-16 16:31:51] [ERROR ENCOUNTERED]: test log value error!
+#>
     [CmdletBinding()]
         param(
             [Parameter(Mandatory = $true,
@@ -324,6 +584,66 @@ function Write-LogFileError {
 }
 
 function Write-LogFileWarning {
+<#
+.SYNOPSIS
+   Write-LogFileWarning will add information to a log file created with New-LogFile. The information will be prepended with [ERROR].
+
+.DESCRIPTION
+   Write-LogFileWarning will add information to a log file created with New-LogFile. The information will be prepended with [ERROR].
+
+   By default additions to the log file will include a timestamp, unless you specify -addTimeStamp $false.
+   This function accepts values from the pipeline, as demonstrated in an example.
+
+   Use the -Verbose parameter to display what is being logged to the host.
+   
+.PARAMETER logPath
+    Alias: Path
+    Type : String
+
+    Specify the full path to the log file, including the name.
+
+.PARAMETER warningDesc
+    Alias: Value
+    Type : String
+
+    Specify the value(s) you'd like logged as errors.
+
+.PARAMETER addTimeStamp
+    Type : Boolean
+
+    Defaults to true, set to false if you'd like to omit the timestamp.
+
+.PARAMETER exitScript
+    Alias: Exit
+    Type : Boolean
+
+    This parameter let's you specify $true if you'd like to exit the script after the error is logged. 
+    It defaults to $false.
+
+.NOTES
+    Name: Write-LogFileWarning
+    Version: 1.0
+    Author: Ginger Ninja (Mike Roberts)
+    DateCreated: 5/11/16
+
+.LINK
+    http://www.gngrninja.com
+
+.EXAMPLE
+    For this example we'll assume you use:
+    $logFile = New-LogFile 
+
+    Write-LogFileWarning -logPath $logFile -warningDesc 'test log value warning!'
+    -----------------------------
+
+    gngrNinja> more $logFile
+    -----------------------------------------------------------------
+    Started logging at [05/11/2016 16:38:29]
+    Script (Version 0.1) executed by: [thegn] on computer: [GINJA10]
+    -----------------------------------------------------------------
+
+    [05-11-16 16:38:48] [WARNING]: test log value warning!
+#>
     [CmdletBinding()]
         param(
             [Parameter(Mandatory = $true,
@@ -400,6 +720,73 @@ function Write-LogFileWarning {
 }
 
 function Resolve-LogFile {
+<#
+.SYNOPSIS
+   Resolve-LogFile will resolve a created log file.
+
+.DESCRIPTION
+   Resolve-LogFile will resolve a created log file.
+
+   Use the -Verbose parameter to display what is happening to the host.
+
+.PARAMETER logPath
+    Alias: Path
+    Type : String
+
+    Specify the full path, including name, to the log file to be resolved.
+
+.PARAMETER logName    
+    Alias: Name
+    Type : String
+
+    Specify the name of the log file. Be sure to include the extension if specifying the name.
+
+.PARAMETER exitScript
+    Alias: Exit
+    Type : Boolean
+
+    Specify $true if you'd like to exit the script after the log file is resolved. 
+    It defaults to $false.
+
+.NOTES
+    Name: Resolve-LogFile
+    Version: 1.0
+    Author: Ginger Ninja (Mike Roberts)
+    DateCreated: 5/11/16
+    
+.LINK
+    http://www.gngrninja.com
+
+.EXAMPLE
+    $logFile = New-LogFile 
+    -----------------------------
+    
+    gngrNinja> $logFile
+    C:\PowerShell\logs\ScriptLog_05-11-16_1612.log
+
+.EXAMPLE
+
+    $logFile = New-LogFile 
+    Get-Process | Write-LogFile $logFile
+
+    Resolve-LogFile $logFile
+    -----------------------------
+    
+    ...
+    [05-11-16 16:43:58] System.Diagnostics.Process (wininit)
+
+    [05-11-16 16:43:58] System.Diagnostics.Process (winlogon)
+
+    [05-11-16 16:43:58] System.Diagnostics.Process (WmiPrvSE)
+
+    [05-11-16 16:43:58] System.Diagnostics.Process (WmiPrvSE)
+
+    [05-11-16 16:43:58] System.Diagnostics.Process (WUDFHost)
+
+    ---------------------------------------------
+    Ended logging at [05/11/2016 16:44:01]
+    ---------------------------------------------
+#>
     [cmdletbinding()]
     param(
         [parameter(Mandatory = $true,
@@ -422,12 +809,12 @@ function Resolve-LogFile {
         
         Write-Verbose "Adding the following content to: $logPath"
         Write-Verbose ('-'*$flairLength)
-        Write-Verbose "Finished processing at [$time]"
+        Write-Verbose "Ended logging at [$time]"
         Write-Verbose ('-'*$flairLength)
         Write-Verbose ""
         
         Add-Content -Path $logPath -Value ('-'*$flairLength)
-        Add-Content -Path $logPath -Value "Finished processing at [$time]"
+        Add-Content -Path $logPath -Value "Ended logging at [$time]"
         Add-Content -Path $logPath -Value ('-'*$flairLength)
    
     } else {
@@ -449,24 +836,116 @@ function Resolve-LogFile {
 }
 
 function Out-LogFile {
+<#
+.SYNOPSIS
+   Out-LogFile will create, add to, and resolve a logfile.
+
+.DESCRIPTION
+   Out-LogFile will create, add to, and resolve a logfile.
+
+   Value from the pipeline is accepted.
+
+   Use the -Verbose parameter to display what is happening to the host.
+
+.PARAMETER logPath
+    Alias: Path
+    Type : String
+
+    Specify the path to the logFile you'd like created.
+
+.PARAMETER logName    
+    Alias: Name
+    Type : String
+
+    Specify the name of the log file. Be sure to include the extension if specifying the name.
+
+.PARAMETER logValue
+    Alias: Value
+    Type : String
+
+    Specify the value(s) you'd like logged.
+
+.PARAMETER addTimeStamp
+    Type : Boolean
+
+    Defaults to true, set to false if you'd like to omit the timestamp.
+
+.NOTES
+    Name: Out-LogFile
+    Version: 1.0
+    Author: Ginger Ninja (Mike Roberts)
+    DateCreated: 5/11/16
+    
+.LINK
+    http://www.gngrninja.com
+
+.EXAMPLE
+    $outLog = Get-Process | Out-LogFile -logPath c:\temp -logName 'outlog.log' -Verbose 
+    -----------------------------
+    
+    VERBOSE: Created c:\temp\outlog.log
+    VERBOSE:
+    VERBOSE: File C:\temp\outlog.log created and verified to exist.
+    VERBOSE:
+    VERBOSE: Adding the following information to: C:\temp\outlog.log
+    VERBOSE:
+    VERBOSE: -----------------------------------------------------------------
+    VERBOSE: Started logging at [05/11/2016 16:58:43]
+    VERBOSE: Script (Version 0.1) executed by: [thegn] on computer: [GINJA10]
+    VERBOSE: -----------------------------------------------------------------
+    VERBOSE:
+    VERBOSE: Adding [05-11-16 16:58:43] System.Diagnostics.Process (AdobeUpdateService) to
+    C:\temp\outlog.log
+    VERBOSE:
+
+.EXAMPLE
+    $outLog = Get-Process | Out-LogFile -Verbose
+    -----------------------------
+    
+    gngrNinja> more $outLog
+    ...
+    [05-11-16 16:56:02] System.Diagnostics.Process (winlogon)
+
+    [05-11-16 16:56:02] System.Diagnostics.Process (WmiPrvSE)
+
+    [05-11-16 16:56:02] System.Diagnostics.Process (WmiPrvSE)
+
+    [05-11-16 16:56:02] System.Diagnostics.Process (WUDFHost)
+
+    ---------------------------------------------
+    Ended logging at [05/11/2016 16:56:02]
+    ---------------------------------------------
+
+gngrNinja>
+#>
     [cmdletbinding()]
     param(
+        [Parameter(Mandatory = $false,
+                   Position  = 0)]
+        [Alias('Path')]
+        [string]
+        $logPath,
+        [Parameter(Mandatory = $false,
+                   Position  = 1)]
+        [Alias('Name')]
+        [string]
+        $logName,
         [Parameter(Mandatory = $true,
                    ValueFromPipeLine               = $true,
                    ValueFromPipelineByPropertyName = $true,
-                   Position                        = 0)]
+                   Position                        = 2)]
         [Alias('Value')]
         [string]
         $logValue,
         [Parameter(Mandatory = $false,
-                   Position  = 1)]
+                   Position  = 3)]
         [boolean]
         $addTimeStamp = $true
     )
     
     Begin {
-    
-        $logFile = New-LogFile
+        
+        $logFile = New-LogFile -logPath $logPath -logName $logName
 
     }
     
